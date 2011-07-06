9a4e4ba75d37b730c29295dc96d1438d9fb3ac76
Tal Yalon <tal.yalon@gmail.com>
2011-07-06 16:18:20 +0300

    added exception handling in the coroutines sync adapter.

--- src/infi/asi/coroutines/sync_adapter.py
+++ src/infi/asi/coroutines/sync_adapter.py
@@ -9,13 +9,18 @@ def sync_wait(generator):
     args = None
     while len(stack) > 0:
         try:
-            result = stack[-1].send(args)
+            if isinstance(args, BaseException):
+                result = stack[-1].throw(args)
+            else:
+                result = stack[-1].send(args)
             args = None
             if isinstance(result, types.GeneratorType):
                 stack.append(result)
             else:
                 args = result
-        # TODO: catch other exceptions and pass them on the stack.
         except StopIteration, e:
             stack.pop()
+        except BaseException, e:
+            stack.pop()
+            args = e
     return result
---
+++ tests/test_sync_coroutines.py
@@ -0,0 +1,48 @@
+from infi.asi.coroutines.sync_adapter import sync_wait
+
+def test_simple_coroutines():
+    def bar():
+        yield 5
+    
+    def foo():
+        i = yield bar()
+        yield i
+
+    res = sync_wait(foo())
+    assert res == 5
+    
+
+def __test_exception():
+    def kar():
+        yield 5
+        
+    def bar():
+        i = yield kar()
+        raise Exception("my bad")
+    
+    def foo():
+        try:
+            i = yield bar()
+        except Exception:
+            i = 7
+        yield i
+
+    res = sync_wait(foo())
+    assert res == 7
+
+def test_using_generator():
+    def kar():
+        return 5
+        
+    def bar():
+        for i in xrange(5):
+            yield i
+
+    def foo():
+        i = yield kar()
+        for n in bar():
+            i += 1
+        yield i
+
+    res = sync_wait(foo())
+    assert res == 10
