4f6539d51119e9caa058f7767106f275120a4c30
Tal Yalon <tal.yalon@gmail.com>
2011-07-06 14:52:30 +0300

    cleaned up and consolidated unix/win examples

--- tests/sync_command_executer_benchmark.py
+++ tests/sync_command_executer_benchmark.py
@@ -1,4 +1,5 @@
-import os
+import platform
+import sys
 import time
 from infi.asi import create_platform_command_executer
 from infi.asi.unix import OSFile
@@ -6,8 +7,24 @@ from infi.asi.cdb.inquiry import StandardInquiryData, StandardInquiryCommand
 from infi.asi.coroutines.sync_adapter import sync_wait
 from infi.exceptools import print_exc
 
+if len(sys.argv) != 2:
+    sys.stderr.write("usage: %s device_name\n" % sys.argv[0])
+    sys.exit(1)
+
+path = sys.argv[1]
+
+if platform.system() == 'Windows':
+    from infi.asi.win32 import OSFile
+    f = OSFile(path,
+               OSFile.GENERIC_READ | OSFile.GENERIC_WRITE,
+               OSFile.FILE_SHARE_READ | OSFile.FILE_SHARE_WRITE,
+               OSFile.OPEN_EXISTING)
+else:
+    import os
+    from infi.asi.unix import OSFile
+    f = OSFile(os.open(path, os.O_RDWR))
+
 try:
-    f = OSFile(os.open("/dev/sg0", os.O_RDWR))
     executer = create_platform_command_executer(f)
 
     iters = 1000
