4664a8c43e5d5dff6437e65bcbf52559749d3c97
Guy Rozendorn <guy@rzn.co.il>
2011-08-12 19:31:26 +0300

    extending tests

--- tests/test_inquiry.py
+++ tests/test_inquiry.py
@@ -5,6 +5,7 @@ from infi.unittest import TestCase
 from infi.unittest.parameters import iterate
 from infi.asi.cdb.inquiry.standard import StandardInquiryCommand
 from infi.asi.cdb.inquiry.vpd_pages import UnitSerialNumberVPDPageCommand, DeviceIdentificationVPDPageCommand
+from infi.asi.cdb.inquiry.vpd_pages import SupportedVPDPagesCommand
 
 class LinuxInquiryTestCase(TestCase):
     def setUp(self):
@@ -12,7 +13,8 @@ class LinuxInquiryTestCase(TestCase):
         if system() != "Linux":
             raise unittest.SkipTest()
 
-    @iterate("command", [StandardInquiryCommand, UnitSerialNumberVPDPageCommand, DeviceIdentificationVPDPageCommand])
+    @iterate("command", [StandardInquiryCommand, UnitSerialNumberVPDPageCommand,
+                         DeviceIdentificationVPDPageCommand, SupportedVPDPagesCommand])
     @iterate("path", glob("/dev/sg*"))
     def test_inquiry_command_on_path(self, path, command):
         from infi.asi import create_platform_command_executer, AsiCheckConditionError
@@ -25,6 +27,8 @@ class LinuxInquiryTestCase(TestCase):
         try:
             _ = sync_wait(cdb.execute(executer))
         except AsiCheckConditionError, e:
+            # Some devices does not support some pages
+            # for example, VMware on hba02 does not support 0x83
             if e.sense_obj.sense_key == 'ILLEGAL_REQUEST' \
                         and e.sense_obj.additional_sense_code.code_name == 'INVALID FIELD IN CDB':
                 pass
