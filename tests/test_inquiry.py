bcb28d1f29a827872562de8f8631ae3406dc1852
Guy Rozendorn <guy@rzn.co.il>
2011-08-12 17:09:51 +0300

    adding inquiry tests

---
+++ tests/test_inquiry.py
@@ -0,0 +1,33 @@
+from __future__ import print_function
+
+from glob import glob
+from infi.unittest import TestCase
+from infi.unittest.parameters import iterate
+from infi.asi.cdb.inquiry.standard import StandardInquiryCommand
+from infi.asi.cdb.inquiry.vpd_pages import UnitSerialNumberVPDPageCommand, DeviceIdentificationVPDPageCommand
+
+class LinuxInquiryTestCase(TestCase):
+    def setUp(self):
+        from platform import system
+        if system() != "Linux":
+            raise unittest.SkipTest()
+
+    @iterate("command", [StandardInquiryCommand, UnitSerialNumberVPDPageCommand, DeviceIdentificationVPDPageCommand])
+    @iterate("path", glob("/dev/sg*"))
+    def test_inquiry_command_on_path(self, path, command):
+        from infi.asi import create_platform_command_executer, AsiCheckConditionError
+        from infi.asi.coroutines.sync_adapter import sync_wait
+        from os import open, O_RDWR
+        from infi.asi.unix import OSFile
+        f = OSFile(open(path, O_RDWR))
+        executer = create_platform_command_executer(f)
+        cdb = command.create()
+        try:
+            _ = sync_wait(cdb.execute(executer))
+        except AsiCheckConditionError, e:
+            if e.sense_obj.sense_key == 'ILLEGAL_REQUEST' \
+                        and e.sense_obj.additional_sense_code.code_name == 'INVALID FIELD IN CDB':
+                pass
+            else:
+                raise
+        f.close()
