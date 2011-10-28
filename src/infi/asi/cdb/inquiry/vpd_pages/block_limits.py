5377f8e64307fe6ebea0a7227534f5a9376e0611
Guy Rozendorn <guy@rzn.co.il>
2011-10-28 15:57:17 +0200

    updating block_limits vpd according to spc3r26

--- src/infi/asi/cdb/inquiry/vpd_pages/block_limits.py
+++ src/infi/asi/cdb/inquiry/vpd_pages/block_limits.py
@@ -1,13 +1,15 @@
-from infi.instruct import Struct, Field, UBInt8, UBInt16, UBInt32, Padding
+from infi.instruct import Struct, Field, UBInt8, UBInt16, UBInt32, UBInt64, BitFields, BitField, BitPadding
 from .. import PeripheralDeviceData
 from . import EVPDInquiryCommand
 
+# spc3r26: 6.5.3 (page 241)
 class BlockLimitsVPDPageData(Struct):
     _fields_ = [
         Field("peripheral_device", PeripheralDeviceData),
         UBInt8("page_code"),
         UBInt16("page_length"),
-        Padding(1),
+        BitFields(BitPadding(7),
+                  BitField("wsnz", 1)),
         UBInt8("maximum_compare_and_write_length"),
         UBInt16("optimal_transfer_length_granularity"),
         UBInt32("maximum_transfer_length"),
@@ -16,7 +18,10 @@ class BlockLimitsVPDPageData(Struct):
         UBInt32("maximum_unmap_lba_count"),
         UBInt32("maximum_unmap_block_descriptor_count"),
         UBInt32("optimal_unmap_granularity"),
-        UBInt32("unmap_granularity_alignment")
+        BitFields(BitField("ugavalid", 1),
+                  BitField("unmap_granularity_alignment", 31)),
+        UBInt64("maximum_write_same_length"),
+        BitFields(BitPadding(21)),
     ]
 
 class BlockLimitsPageCommand(EVPDInquiryCommand):
