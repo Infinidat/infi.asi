f3a2ec66ced91f2941fcea333f8fde8e0bb925e6
Guy Rozendorn <guy@rzn.co.il>
2012-03-15 17:20:09 +0200

    Fixing UnknownVPDPageCommand

--- src/infi/asi/cdb/inquiry/vpd_pages/unknown.py
+++ src/infi/asi/cdb/inquiry/vpd_pages/unknown.py
@@ -11,9 +11,11 @@ class UnknownVPDPageData(Struct):
    ]
 
 # spc4r30: 7.8.15
-class UnknownVPDPageCommand(EVPDInquiryCommand):
-    def __init__(self, page_code):
-        super(UnknownVPDPageCommand, self).__init__(page_code, 255, UnknownVPDPageData)
+def UnknownVPDPageCommand(page_code):
+    class UnknownVPDPageCommand(EVPDInquiryCommand):
+        def __init__(self):
+            super(UnknownVPDPageCommand, self).__init__(page_code, 255, UnknownVPDPageData)
+    return UnknownVPDPageCommand
 
 __all__ = ["UnknownVPDPageCommand", "UnknownVPDPageData"]
 
