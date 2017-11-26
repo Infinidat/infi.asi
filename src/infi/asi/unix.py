63c847e9fccf29b462f228e9b74d8bdb5b1fce30
Amit Oren <aoren@infinidat.com>
2017-11-26 21:16:52 +0200

    STORAGEMODEL-403 infi.asi: add timeout to UnixFile read so we won't wait indefinitely

--- src/infi/asi/unix.py
+++ src/infi/asi/unix.py
@@ -3,13 +3,15 @@ import select
 
 from . import OSFile
 
+READ_TIMEOUT = 60    # seconds, more than enough
+
 
 class UnixFile(OSFile):
     def __init__(self, fd):
         self.fd = fd
 
     def read(self, size):
-        while not select.select([self.fd], [], [])[0]:
+        while not select.select([self.fd], [], [], READ_TIMEOUT)[0]:
             pass
         return os.read(self.fd, size)
 
