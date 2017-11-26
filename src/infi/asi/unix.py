2a5b98b41ae1ed10b82b6ca2dcbf899600efe4b6
Amit Oren <aoren@infinidat.com>
2017-11-26 14:07:44 +0200

    STORAGEMODEL-403 infi.asi: UnixFile.read should wait until device is ready

--- src/infi/asi/unix.py
+++ src/infi/asi/unix.py
@@ -1,12 +1,16 @@
 import os
+import select
 
 from . import OSFile
+
 
 class UnixFile(OSFile):
     def __init__(self, fd):
         self.fd = fd
 
     def read(self, size):
+        while not select.select([self.fd], [], [])[0]:
+            pass
         return os.read(self.fd, size)
 
     def write(self, buffer):
